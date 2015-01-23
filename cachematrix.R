fe648bf0e358968d45ce61cce054080e312913a6
moatza <micleax@gmail.com>
2015-01-22 21:43:16 -0800

    save first version for homework functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Create a "matrix" object 
+## initialize with NULL the invese matrix
+makeCacheMatrix <- function(x = matrix()) 
+{
+  ## mat variable contains the matrix we consider the reference matrix
+  
+  ##TestCase 1 -  build a matrix using the matrix function
+  ## mat<<- matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2, byrow = TRUE)
+  
+  ##TestCase 2 -  build a matrix using the matrix function
+  mat<<- matrix(c(3,4,5,6), nrow = 2, ncol = 2, byrow = TRUE)
+  
+  ##init the variable where we save the inverse
+  inv <<- NULL
+  
+  return(mat)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'(where x is the param passed to the function)
+## assumes that the matrix passed as parameter has an inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## calculate the inv for the 1st time
+  if (is.null(inv))
+  {
+    inv <<- solve(x)
+    message("calculate inverse for 1st time")
+  }
+  ## calculate the inverse if parameter x is a matrix different from the matrix we compare it with saved in variable 'mat'
+  else
+    if(!identical(x, mat))
+    {
+      message("matrix changed, recalculating the inverse")
+      ## update the base matrix variable, 'mat'
+      mat<<-x
+      inv <<- solve(x)
+    }
+    else
+    {
+      message("same matrix, using the cashed inverse")
+    }
+  
+  #return the inverse 
+  return (inv)
 }
